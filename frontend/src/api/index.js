d475ac7b74c394278a6619b944a8ffa63bc4e6ae
Nitesh Giri <niteshgiri@Niteshs-MacBook-Air.local>
2025-10-27 20:11:26 +0530

    add backend url

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8008/api';
+const API_URL = 'https://cipher-studio-pqb5d3unl-nitesh-giris-projects.vercel.app/';
 
 const api = axios.create({
   baseURL: API_URL,
